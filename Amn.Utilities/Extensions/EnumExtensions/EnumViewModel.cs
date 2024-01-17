4d3116e29e7a347637690f0e07f43dda12cedee7
abbas1973 <abbas.mn1973@gmail.com>
2024-01-17 18:48:31 +0330

    Add project files.

---
+++ Amn.Utilities/Extensions/EnumExtensions/EnumViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Amn.Utilities
+{
+
+
+    /// <summary>
+    /// مدل برای تبدیل enum به کلاس
+    /// </summary>
+    public class EnumVM
+    {
+        public int? Id { get; set; }
+        public string Key { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Amn.Utilities/Extensions/SecurityExtension.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Amn.Utilities
+{
+    public static class SecurityExtension
+    {
+        public static string GetHash(this string pass, string salt = null)
+        {
+            if(salt != null)
+                pass += salt;
+            else
+                pass += "*3^z$i8)*&";
+            byte[] data = Encoding.ASCII.GetBytes(pass);
+
+            #region هش با استفاده از MD5
+            //using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            //{
+            //    data = md5.ComputeHash(data);
+            //}
+            #endregion
+
+
+            #region هش با استفاده از SHA512 - روش جدید که 128 کاراکتر هگز میده
+            using (var hash = SHA512.Create())
+            {
+                var hashedInputBytes = hash.ComputeHash(data);
+
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashedInputBytes)
+                    sb.Append(b.ToString("X2"));
+
+                var encPass = sb.ToString();
+                return encPass;
+            }
+            #endregion
+
+        }
+
+    }
+}
